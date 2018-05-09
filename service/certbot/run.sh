8a781dce4c1d2644a541d60bf8e804ec449d9a60
Wator Vapor <watorvapor@gmail.com>
2018-05-09 13:08:47 +0900

    Update run.sh

--- service/certbot/run.sh
+++ service/certbot/run.sh
@@ -1,6 +1,2 @@
 #!/bin/sh
-certbot certonly --manual -d '*.wator.xyz' -d wator.xyz -m 'watorvapor@gmail.com' \
-  --agree-tos --preferred-challenges dns-01 \
-  --server https://acme-v02.api.letsencrypt.org/directory
 sleep 60
-
