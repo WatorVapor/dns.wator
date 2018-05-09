c67c9d471812b583d5062cca4c0278004ee98fb3
Wator Vapor <watorvapor@gmail.com>
2018-05-09 13:19:03 +0900

    Update create.sh

--- service/certbot/create.sh
+++ service/certbot/create.sh
@@ -5,6 +5,6 @@ docker run \
   -v log:/var/log/letsencrypt \
   certbot/certbot \
   certonly --manual -d '*.wator.xyz' -d wator.xyz -m 'watorvapor@gmail.com' \
-  --agree-tos --preferred-challenges dns-01 \
+  --preferred-challenges dns-01 \
   --server https://acme-v02.api.letsencrypt.org/directory
 
