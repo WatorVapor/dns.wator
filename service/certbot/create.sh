8cfa5866d534f41a25871eeaeb679809ec9d59d2
Vapor Wator <watorvapor@gmail.com>
2018-05-09 11:16:02 +0900

    wild domain

--- service/certbot/create.sh
+++ service/certbot/create.sh
@@ -3,4 +3,5 @@ find /etc/
 certbot certonly --manual -d '*.wator.xyz' -d wator.xyz -m 'watorvapor@gmail.com' \
   --agree-tos --preferred-challenges dns-01 \
   --server https://acme-v02.api.letsencrypt.org/directory
+
   
