2017da653eff820698135812c9b12863b583d0d0
Wator Vapor <watorvapor@gmail.com>
2018-08-07 18:09:52 +0900

    Update renew.sh

--- service/certbot/renew.sh
+++ service/certbot/renew.sh
@@ -4,7 +4,7 @@ docker run \
   -v /opt/dns.wator/service/certbot/var:/var/lib/letsencrypt \
   -v /opt/dns.wator/service/certbot/log:/var/log/letsencrypt \
   -it certbot/certbot \
-  certonly --renew -d '*.wator.xyz' -d wator.xyz -m 'watorvapor@gmail.com' \
+  renew -d '*.wator.xyz' -d wator.xyz -m 'watorvapor@gmail.com' \
   --preferred-challenges dns-01 \
   --server https://acme-v02.api.letsencrypt.org/directory
 
