853c66fb98d3cb60553b293bfd3ed45b16684f18
Wator Vapor <watorvapor@gmail.com>
2020-08-20 10:07:24 +0900

    Update create.sh

--- service/certbot/create.sh
+++ service/certbot/create.sh
@@ -5,7 +5,7 @@ docker run \
   -v /opt/dns.wator/service/certbot/log:/var/log/letsencrypt \
   -it certbot/certbot \
   certonly -d '*.wator.xyz' -d wator.xyz -m 'watorvapor@gmail.com' \
-  –manual-public-ip-logging-ok \
+  --manual-public-ip-logging-ok --agree-tos \
   --preferred-challenges dns-01 \
   --server https://acme-v02.api.letsencrypt.org/directory \
   --manual
